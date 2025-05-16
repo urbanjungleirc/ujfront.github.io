b050423f9b08a43aab977e0cb5bceaea2e72c440
Jiri Stastny <jiri@urbanjungleirc.com>
2025-05-16 17:03:55 +0800

    v10

--- HVT/js/api.js
+++ HVT/js/api.js
@@ -2,32 +2,22 @@
 const GAS_ENDPOINT =
     "https://script.google.com/a/macros/urbanjungleirc.com/s/AKfycbx8fO-LDqXnhAQO2N_TJRrFRIPZDqR9rRaYofkAyKBWOPW6D49e6jlN-daA-0oyXMmWWQ/exec";
 
-/**
- * Generic function to call the backend with the specified action and parameters.
- */
-function callBackend(action, params = {}) {
-    const url = new URL(GAS_ENDPOINT);
-    url.searchParams.append("action", action);
-    Object.keys(params).forEach((key) => {
-        url.searchParams.append(key, params[key]);
-    });
-    return fetch(url.toString()).then((response) => response.json());
+
+function callBackend(action, params={}) {
+  const url = new URL(GAS_ENDPOINT);
+  url.searchParams.append("action", action);
+  Object.entries(params).forEach(([k,v])=> url.searchParams.append(k,v));
+  return fetch(url).then(r=>r.json());
 }
 
-// Expose API calls on the global "api" object.
 window.api = {
-    getConfig: (className, scoringSystem) =>
-        callBackend("getConfig", { class: className, system: scoringSystem }),
-    getResultsData: () => callBackend("getResultsData"),
-    resetSession: () => callBackend("resetSession"),
-    moveData: (newSheetName) => callBackend("moveData", { newSheetName }),
-    addStudent: (studentName, studentClass) =>
-        callBackend("addStudent", { studentName, studentClass }),
-    addScoringSystem: (systemName, scoringType, scoringValue) =>
-        callBackend("addScoringSystem", {
-            systemName,
-            scoringType,
-            scoringValue,
-        }),
-    // You can add more API functions as required.
+  getConfig:        (cls,sys) => callBackend("getConfig",        {class:cls, system:sys}),
+  getResultsData:   ()       => callBackend("getResultsData"),
+  resetSession:     ()       => callBackend("resetSession"),
+  moveData:         (name)   => callBackend("moveData",         {newSheetName:name}),
+  addStudent:       (n,c)    => callBackend("addStudent",       {studentName:n, studentClass:c}),
+  addScoringSystem: (n,t,v)  => callBackend("addScoringSystem",{systemName:n, scoringType:t, scoringValue:v}),
+  getSavedSessions: ()       => callBackend("getSavedSessions"),
+  loadSession:      (name)   => callBackend("loadSession",      {sheetName:name})
 };
+
